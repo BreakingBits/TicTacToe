869da42bb39c72c58f90dafb8cad93b0deef9712
Kobblander <kobblander@gmail.com>
2013-11-18 17:19:04 +0000

    Added functionality for GameBoard class and tests for GameboardTest class which test diagonal rows.

--- src/main/java/com/breakingbits/app/GameBoard.java
+++ src/main/java/com/breakingbits/app/GameBoard.java
@@ -6,23 +6,37 @@ public class GameBoard
 {
  	public static Boolean checkIfWinner(int[] grid, int symbol){
  		//In the grid: Zero is empty, One is X, Two is Circle.
- 		//Horizontal win.
+ 		//---Horizontal win---
  		if(((grid[0] == symbol) && 
  			(grid[1] == symbol) && 
  			(grid[2] == symbol))){
  			return true;
  		}
- 		else if(((grid[3] == symbol) && 
- 			     (grid[4] == symbol) && 
- 			     (grid[5] == symbol))){
+ 		if(((grid[3] == symbol) && 
+ 		    (grid[4] == symbol) && 
+ 		    (grid[5] == symbol))){
  			return true;
  		}
- 		else if(((grid[6] == symbol) && 
- 			     (grid[7] == symbol) && 
- 			     (grid[8] == symbol))){
+ 		if(((grid[6] == symbol) && 
+    	    (grid[7] == symbol) && 
+ 		    (grid[8] == symbol))){
  			return true;
  		}
- 		
+ 		//--------------------
+
+ 		//----Diagonal win----
+ 		if(((grid[0] == symbol) && 
+    	    (grid[4] == symbol) && 
+ 		    (grid[8] == symbol))){
+ 			return true;
+ 		}
+ 		if(((grid[6] == symbol) && 
+    	    (grid[4] == symbol) && 
+ 		    (grid[2] == symbol))){
+ 			return true;
+ 		}
+ 		//--------------------
+
  		return false;
  	} 
 }
--- src/test/java/com/breakingbits/app/GameBoardTest.java
+++ src/test/java/com/breakingbits/app/GameBoardTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class GameBoardTest
 {
     @Test
-    public void testIHorizontalWinIsTrue(){
+    public void testIfHorizontalWinIsTrueFirstRow(){
  		//In the grid: Zero is empty, One is X, Two is Circle.
 
     	int[] grid1 = {1, 1, 1,
@@ -18,7 +18,55 @@ public class GameBoardTest
     }
 
     @Test
-    public void testIfHorizontalWinIsFalse(){
+    public void testIfHorizontalWinIsTrueSecondRow(){
+ 		//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid1 = {0, 0, 0,
+    			 	   1, 1, 1,
+    			       0, 0, 0};
+    	
+    	int symbolX = 1;
+    	assertEquals(true, GameBoard.checkIfWinner(grid1, symbolX));
+    }
+
+    @Test
+    public void testIfHorizontalWinIsTrueThirdRow(){
+ 		//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid1 = {0, 0, 0,
+    			 	   0, 0, 0,
+    			       1, 1, 1};
+    	
+    	int symbolX = 1;
+    	assertEquals(true, GameBoard.checkIfWinner(grid1, symbolX));
+    }
+
+    @Test
+    public void testIfDiagonalWinIsTrueRight(){
+    	//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid1 = {1, 0, 0,
+    			 	   0, 1, 0,
+    			       0, 0, 1};
+    	
+    	int symbolX = 1;
+    	assertEquals(true, GameBoard.checkIfWinner(grid1, symbolX));
+    }
+
+    @Test
+    public void testIfDiagonalWinIsTrueLeft(){
+    	//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid1 = {0, 0, 1,
+    			 	   0, 1, 0,
+    			       1, 0, 0};
+    	
+    	int symbolX = 1;
+    	assertEquals(true, GameBoard.checkIfWinner(grid1, symbolX));
+    }
+
+    @Test
+    public void testIfWinIsFalse(){
  		//In the grid: Zero is empty, One is X, Two is Circle.
 
     	int[] grid2 = {0, 0, 0,

dbfc2a92a163a8e953b8c5a7d72c5cefaab43cad
brynjar <brysbra@gmail.com>
2013-11-18 14:11:40 +0000

    Started a little on the Player and TicTacToe class.

--- src/main/java/com/breakingbits/app/Player.java
+++ src/main/java/com/breakingbits/app/Player.java
@@ -4,5 +4,22 @@ import spark.*;
 
 public class Player
 {
-    
+    private String name;
+    private String symbol;
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public Player() {
+    	name = "";
+    	symbol = "";
+    	wins = 0;
+    	losses = 0;
+    	ties = 0;
+    }
+
+    public void setName (String nm) {
+    	name = nm;
+    }
+
 }
--- src/main/java/com/breakingbits/app/TicTacToe.java
+++ src/main/java/com/breakingbits/app/TicTacToe.java
@@ -2,7 +2,7 @@ package com.breakingbits.app;
 import static spark.Spark.*;
 import spark.*;
 
-public class TicTacToe 
+public class TicTacToe
 {
     public static void main( String[] args )
     {
@@ -14,5 +14,24 @@ public class TicTacToe
             return "Hello World!";
          }
         });
+
+        final Player PlayerOne = new Player();
+        final Player PlayerTwo = new Player();
+
+        get(new Route("/insertName") {
+            @Override
+            public Object handle(Request request, Response response) {
+
+                String playerOneName = String.valueOf(request.queryParams("playerOne"));
+                String playerTwoName = String.valueOf(request.queryParams("playerTwo"));
+
+                PlayerOne.setName(playerOneName);
+                PlayerTwo.setName(playerTwoName);
+
+                return 0;
+            }
+        });
+      
+        
     }
 }

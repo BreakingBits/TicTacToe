ee7ff2388f56730a2696c369fd4a360d21409f7d
Kobblander <kobblander@gmail.com>
2013-11-18 17:02:19 +0000

    Next iteration in GameBoard test class. Some functionality added.

--- src/test/java/com/breakingbits/app/GameBoardTest.java
+++ src/test/java/com/breakingbits/app/GameBoardTest.java
@@ -6,7 +6,25 @@ import org.junit.Test;
 public class GameBoardTest
 {
     @Test
-    public void testIfWinnerIsTrue(){
-    	assertEquals(true, GameBoard.checkIfWinner(true));
+    public void testIHorizontalWinIsTrue(){
+ 		//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid1 = {1, 1, 1,
+    			 	   0, 0, 0,
+    			       0, 0, 0};
+    	
+    	int symbolX = 1;
+    	assertEquals(true, GameBoard.checkIfWinner(grid1, symbolX));
+    }
+
+    @Test
+    public void testIfHorizontalWinIsFalse(){
+ 		//In the grid: Zero is empty, One is X, Two is Circle.
+
+    	int[] grid2 = {0, 0, 0,
+    			 	   0, 0, 0,
+    				   0, 0, 0};
+    	int symbolX = 1;
+    	assertEquals(false, GameBoard.checkIfWinner(grid2, symbolX));
     }
 }
